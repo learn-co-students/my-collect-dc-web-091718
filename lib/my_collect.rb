eb40a52306c1c57c0283e9edc1d2e1030126510b
cd <coraldarling@outlook.com>
2018-09-05 16:47:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+    returned_array = []
+    
+    while i < array.length
+      returned_array.push yield array[i]
+      i += 1
+      
+    end
+    returned_array
+  else
+    "No collection given, none returned."
+  end
+end  
 
 
